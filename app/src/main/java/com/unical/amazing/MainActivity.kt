74b18e46fb0c0be4e0322d3fee4344ebf5e827fc
Luca <lucacastagna19@gmail.com>
2024-05-13 13:22:36 +0200

    code update

--- app/src/main/java/com/unical/amazing/MainActivity.kt
+++ app/src/main/java/com/unical/amazing/MainActivity.kt
@@ -8,18 +8,22 @@ import androidx.activity.enableEdgeToEdge
 import com.unical.amazing.theme.AmazingTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+
 
 
 class MainActivity : ComponentActivity() {
@@ -27,9 +31,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         setContent {
             AmazingTheme {
-                Navbar()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Scaffold(
+                        bottomBar = { NavBar() },
+                        topBar = { SearchBar() }
+                    ) {}
+                }
             }
         }
     }
@@ -37,17 +50,18 @@ class MainActivity : ComponentActivity() {
 
 
 
-data class BottomNavigationItem(
-    val title: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector,
-    val cartEmpty: Boolean,
-    val badgeCount: Int? = null
-)
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+
+//Funzioni per generare la barra di navigazione e la barra di ricerca
 @Composable
-fun Navbar(){
+fun NavBar(){
+    data class BottomNavigationItem(
+        val title: String,
+        val selectedIcon: ImageVector,
+        val unselectedIcon: ImageVector,
+        val cartEmpty: Boolean,
+        val badgeCount: Int? = null
+    )
     val items = listOf(
         BottomNavigationItem(
             title = "Home",
@@ -68,56 +82,62 @@ fun Navbar(){
             cartEmpty = true,
             badgeCount = 0
         ))
-
     var selectedItemIndex by rememberSaveable {
-        mutableStateOf(0)
+        mutableIntStateOf(0)
     }
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
+    NavigationBar(
+            containerColor = Color.White
     ) {
-        Scaffold(
-            bottomBar = {
-                NavigationBar(
-                    containerColor = Color.White
-                ) {
-                    items.forEachIndexed { index, item ->
-                        NavigationBarItem(
-                            selected = selectedItemIndex == index,
-                            onClick = {
-                                selectedItemIndex = index
-                            },
-                            label ={
-                                Text(text = item.title)
-                            },
-                            icon = {
-                                BadgedBox(
-                                    badge ={
-                                        if (item.badgeCount != null) {
-                                            Badge {
-                                                Text(text = item.badgeCount.toString())
-                                            }
-                                        }
-                                        else if (!item.cartEmpty) {
-                                            Badge()
-                                        }
-                                    } ) {
-                                    Icon(
-                                        contentDescription = item.title,
-                                        imageVector = if (index== selectedItemIndex) item.unselectedIcon else item.selectedIcon
-                                    )
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    selected = selectedItemIndex == index,
+                    onClick = {
+                        selectedItemIndex = index
+                    },
+                    label = {
+                        Text(text = item.title)
+                    },
+                    icon = {
+                        BadgedBox(
+                            badge = {
+                                if (item.badgeCount != null) {
+                                    Badge {
+                                        Text(text = item.badgeCount.toString())
+                                    }
+                                } else if (!item.cartEmpty) {
+                                    Badge()
                                 }
-                            }
-                        )
+                            }) {
+                            Icon(
+                                contentDescription = item.title,
+                                imageVector = if (index == selectedItemIndex) item.unselectedIcon else item.selectedIcon
+                            )
+                        }
                     }
-                }
-            }){}
-    }
+                )
+            }
+        }
+}
+
+
+@Composable
+fun SearchBar(){
+    TopAppBar(
+        title = { Text(text = "App Title") },
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = Color.White,
+        elevation = 4.dp,
+        navigationIcon = { /* Icona di navigazione, se necessario */ },
+        actions = {
+            // Campo di ricerca
+            IconButton(onClick = { /* Azione di ricerca */ }) {
+                Icon(Icons.Filled.Search, contentDescription = "Search")
+            }
+        }
+    )
 }
 
 
 
-@Composable
-@Preview
-fun tryit(){ Navbar()}
+
