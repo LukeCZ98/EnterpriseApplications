044d99b4af5c983e30feefabc1a0ff3407b4fd4d
Luca Foscolo <120180029+luca-foscolo@users.noreply.github.com>
2024-07-10 12:43:10 +0200

    Update EmailNotFoundException.java

--- src/main/java/unical/informatica/it/enterpriseapplicationbackend/exception/EmailNotFoundException.java
+++ src/main/java/unical/informatica/it/enterpriseapplicationbackend/exception/EmailNotFoundException.java
@@ -1,7 +1,1 @@
-package unical.informatica.it.enterpriseapplicationbackend.exception;
 
-/**
- * Exception thrown when an email address given could not be found.
- */
-public class EmailNotFoundException extends Exception {
-}
