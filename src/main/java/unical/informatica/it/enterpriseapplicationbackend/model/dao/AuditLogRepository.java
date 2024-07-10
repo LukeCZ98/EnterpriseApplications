64e939cf8b75cc8cc2996e9c1aefcc86cc102291
Luca Foscolo <120180029+luca-foscolo@users.noreply.github.com>
2024-07-10 15:20:10 +0200

    Update AuditLogRepository.java

--- src/main/java/unical/informatica/it/enterpriseapplicationbackend/model/dao/AuditLogRepository.java
+++ src/main/java/unical/informatica/it/enterpriseapplicationbackend/model/dao/AuditLogRepository.java
@@ -1,8 +1,1 @@
-package unical.informatica.it.enterpriseapplicationbackend.model.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import unical.informatica.it.enterpriseapplicationbackend.model.AuditLog;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-    AuditLog findTopByOrderByIdDesc();
-}
