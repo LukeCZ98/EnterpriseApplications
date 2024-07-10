6959942d9e7690fdf9fb0a5a6beeb29500096f6b
Luca Foscolo <120180029+luca-foscolo@users.noreply.github.com>
2024-07-10 12:54:40 +0200

    Update LoginResponse.java

--- src/main/java/unical/informatica/it/enterpriseapplicationbackend/api/model/LoginResponse.java
+++ src/main/java/unical/informatica/it/enterpriseapplicationbackend/api/model/LoginResponse.java
@@ -1,1 +1,49 @@
+package unical.informatica.it.enterpriseapplicationbackend.api.model;
+
+/**
+ * The response object sent from login request.
+ */
+public class LoginResponse {
+
+  /** The JWT token to be used for authentication. */
+  private String jwt;
+  /** Was the login process successful? */
+  private boolean success;
+  /** The reason for failure on login. */
+  private String failureReason;
+
+  private Boolean role;
+
+  public String getJwt() {
+    return jwt;
+  }
+
+  public void setJwt(String jwt) {
+    this.jwt = jwt;
+  }
+
+  public boolean isSuccess() {
+    return success;
+  }
+
+  public void setSuccess(boolean success) {
+    this.success = success;
+  }
+
+  public String getFailureReason() {
+    return failureReason;
+  }
+
+  public void setFailureReason(String failureReason) {
+    this.failureReason = failureReason;
+  }
+
+  public Boolean getRole() {
+    return role;
+  }
+  public void setRole(Boolean role) {
+    this.role = role;
+  }
+
+}
 
