f3b8ab0b018eb35a3725f4d9cad021bffa73f39a
MooDifferenT <102440421+MooDifferenT@users.noreply.github.com>
2024-07-10 12:13:54 +0200

    Update ProductService.java
    nn

--- src/main/java/unical/informatica/it/enterpriseapplicationbackend/service/ProductService.java
+++ src/main/java/unical/informatica/it/enterpriseapplicationbackend/service/ProductService.java
@@ -1,1 +1,77 @@
+package unical.informatica.it.enterpriseapplicationbackend.service;
+
+import jakarta.transaction.Transactional;
+import unical.informatica.it.enterpriseapplicationbackend.api.model.ProductBody;
+import unical.informatica.it.enterpriseapplicationbackend.model.Product;
+import unical.informatica.it.enterpriseapplicationbackend.model.dao.ProductDAO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service for handling product actions.
+ */
+@Service
+public class ProductService {
+
+  /** The Product DAO. */
+  private ProductDAO productDAO;
+
+  /**
+   * Constructor for spring injection.
+   * @param productDAO
+   */
+  public ProductService(ProductDAO productDAO) {
+    this.productDAO = productDAO;
+  }
+
+  //da mettere i dati che l'admin dovrebbe modificare
+  public Boolean updateProduct(Product product) {
+    Optional<Product> prod = productDAO.find(product.getId());
+    if (prod.isPresent()) {
+      Product prd = prod.get();
+      prd.setDescription(product.getDescription());
+      prd.setTitle(product.getTitle());
+      prd.setPrice(product.getPrice());
+      prd.setAvailable(product.getAvailable());
+      productDAO.save(prd);
+      return true;
+    }
+    return false;
+  }
+
+  //elimina prodotto
+  public void deleteProduct(Product product) {
+    Optional<Product> prod = productDAO.find(product.getId());
+      prod.ifPresent(value -> productDAO.delete(value));
+  }
+
+
+  public Product addProduct(Product product) {
+    return productDAO.save(product);
+  }
+
+  public Product findById(Long id) {
+    if(productDAO.find(id).isPresent())
+      return productDAO.find(id).get();
+    else
+      return null;
+  }
+
+
+
+  /**
+   * Gets the all products available.
+   * @return The list of products.
+   */
+  public List<Product> getProducts() {
+    return productDAO.findAll();
+  }
+
+  public List<Product> getProdsbyname(String name) {
+    return productDAO.findByTitleLike(name);
+  }
+
+}
 
