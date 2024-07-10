24e4000cbe8b8fb552f846e58bb4f0b7220a1dbc
Luca Foscolo <120180029+luca-foscolo@users.noreply.github.com>
2024-07-10 12:43:01 +0200

    Update EmailFailureException.java

--- src/main/java/unical/informatica/it/enterpriseapplicationbackend/exception/EmailFailureException.java
+++ src/main/java/unical/informatica/it/enterpriseapplicationbackend/exception/EmailFailureException.java
@@ -1,1 +1,8 @@
+package unical.informatica.it.enterpriseapplicationbackend.exception;
+
+/**
+ * Exception to highlight that we were unable to send an email.
+ */
+public class EmailFailureException extends Exception {
+}
 
